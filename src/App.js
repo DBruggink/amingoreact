c72705ce9dc1b1605ec7f1075f235471a7bf279e
Lordsmokesalot <djls.bruggink@gmail.com>
2020-03-04 19:52:44 +0400

    blah

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, Component} from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './pages/landingpage'
 import Login from './pages/login'
@@ -24,39 +24,20 @@ function App() {
      
     }
   )
-  const LayoutRoute = ({ location, path, exact, component }) => {
-    return (
-        <div>
-          
-            <Navbar location={location.pathname} />
-            <Route 
-                path={path} 
-                exact={exact}
-                component={component}
-            />
-            <Footer location={location.pathname} />
-        </div>
-    )
-}
-/*const PrivateRoute=({component: Component, ...restOfProps})=>{
-  return(
-
-  )
-}*/
 
   return (
     <AppContext.Provider value={[globalState,setGlobalState]}>
 <BrowserRouter>
     <div className='App'>
-  
+  <Navbar/>
   <Switch>
    
-    <LayoutRoute exact path='/' component={Home} />
-    <LayoutRoute path='/login' component={Login} />
-    <LayoutRoute path='/register' component={Registration} />
-    <LayoutRoute path='/about' component={About} />
-    <LayoutRoute path='/profile' component={Profile} />
-    <LayoutRoute path='/feed' component={Feed}/>
+    <Route exact path='/' component={Home} />
+    <Route path='/login' component={Login} />
+    <Route path='/register' component={Registration} />
+    <Route path='/about' component={About} />
+    <Route path='/profile' component={Profile} />
+    <Route path='/feed' component={Feed}/>
 
   </Switch>
   <Footer/>
